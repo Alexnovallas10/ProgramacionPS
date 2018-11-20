1a783f031b10af422646dd5992316e81ee1e0ee2
Alex <alexnovallas10@gmail.com>
2018-11-20 19:34:05 +0100

    Ejercicio contar caracteres de un fichero

--- src/P02_Hilos/Ejercicio_Ficheros.java
+++ src/P02_Hilos/Ejercicio_Ficheros.java
@@ -9,30 +9,24 @@ public class Ejercicio_Ficheros {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		int contador=0;
+		int x, contador=0;
 		long t_inicio, t_fin;
 		long tiempo_total;
-		for(int i=1;i<5;i++) {
-			File f = new File("D:\\Archivos de programa\\workspace\\Procesos\\src\\P02_Hilos\\ficheros\\"+i+".txt");
+		for(int i=1;i<=5;i++) {
+			File f = new File("C:\\Users\\ifc\\git\\ProgramacionPS\\src\\P02_Hilos\\ficheros\\"+i+".txt");
 			FileReader fic = new FileReader(f);
-			while((i=fic.read())!=-1) {
+			while((x=fic.read())!=-1) {
 				contador++;			
 				}
-		
+			System.out.println("Fichero "+i+" :"+contador+" caracteres");
+			fic.close();
 		}
 	
-		
-	
-
-		
-		
-		
-
-		
-	
 		t_inicio = System.currentTimeMillis(); 
-	
 		t_fin = System.currentTimeMillis();
+		tiempo_total=t_fin - t_inicio;
+		System.out.println("Tiempo: "+tiempo_total);
+
 		
 
 	}
