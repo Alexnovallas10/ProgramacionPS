d15c1a41b09bff1b572ca761a145682f51685917
Alex <alexnovallas10@gmail.com>
2018-12-13 18:43:18 +0100

    Comuniacaiones en Red Cliente Servidor

--- src/P03_ComunicacionEnRed/Ej_02_Cliente.java
+++ src/P03_ComunicacionEnRed/Ej_02_Cliente.java
@@ -1,10 +1,30 @@
 package P03_ComunicacionEnRed;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Ej_02_Cliente {
+	
+	String texto = "";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		Scanner teclado = new Scanner(System.in);
+		
+		
+		try {
+			//crea el soket
+			Socket sCliente = new Socket(InetAddress.getLocalHost(), 1234);
+			
+				
+			
+			OutputStream salida = sCliente.getOutputStream();
+		}catch(Exception e) {
+			System.out.println("");
+		}
 	}
 
 }
